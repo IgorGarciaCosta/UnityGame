bd798ef78bdbeb526a6ef52fa6244a217da84c4c
IgorGarciaCosta <igonildo7@gmail.com>
2020-08-20 10:18:32 -0300

    first game changes

---
+++ Assets/scripts/apontarParaInimigo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class apontarParaInimigo : MonoBehaviour
+{
+    [SerializeField]
+    Transform alvo;
+    Vector3 v3_alvo;//vai pegar o posicionamento do personagem
+    
+
+    // Update is called once per frame
+    //obs: Update trabalha cada frame, mas o fixedUpdate trabalha melhor com física
+    void FixedUpdate()
+    {
+        //instância de Vector3
+        v3_alvo = new Vector3(alvo.position.x,alvo.position.y,alvo.position.z);
+        transform.LookAt(v3_alvo);//manda o canhão olhar na posição do personagem
+    }
+}
