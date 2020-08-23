e1c6008b7721292c8c61639f33f9cfefd943b195
IgorGarciaCosta <igonildo7@gmail.com>
2020-08-23 14:19:06 -0300

    giroDaMoeda_BalaDoCanhao

---
+++ Assets/scripts/canhaoAtira.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class canhaoAtira : MonoBehaviour
+{
+    [SerializeField]//pra variável poder se editável dentro do Unity
+    GameObject balaCanhao;
+    [SerializeField]
+    Transform disparador;
+    public float tempoEntreTiros = 3f;//atira a cada 3 segundos
+    private float cronometro =0;
+
+
+
+    void Atirar(){//instacia uma balad de cahão na posição
+        cronometro = 0;//a cada tiro o cronometro zera
+        Instantiate(balaCanhao, disparador.position, disparador.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cronometro += Time.deltaTime;//incrementa 1 segundo ao tempo no cronometro
+
+        if(cronometro >=3){//passados 3 segundos, atirar
+            Atirar();
+        }
+    }
+}
---
+++ Assets/scripts/giraItem.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class giraItem : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        //rotaciona o eixo Y de 60 em 60 graus
+        transform.Rotate(new Vector3(0, 60,0)*Time.deltaTime);//Time.deltaTime faz o giro durar 1 segundo
+        
+    }
+}
